496492847c692fb988bd135a83805305756d7f08
thomas <you@example.com>
2022-03-31 12:20:51 +0200

    lancement app ok

--- src/main/java/com/M2IProject/eventswipe/model/EventEntity.java
+++ src/main/java/com/M2IProject/eventswipe/model/EventEntity.java
@@ -1,7 +1,6 @@
 package com.M2IProject.eventswipe.model;
 
 import java.util.Calendar;
-import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
--- src/main/java/com/M2IProject/eventswipe/model/UsersEventListEntity.java
+++ src/main/java/com/M2IProject/eventswipe/model/UsersEventListEntity.java
@@ -1,15 +1,28 @@
 package com.M2IProject.eventswipe.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "users_eventlist")
 public class UsersEventListEntity {
+	@Id
 	@Column
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
@@ -19,4 +32,5 @@ public class UsersEventListEntity {
 	
 	@Column(name = "event_id")
     private @NotBlank String EventId;
+		
 }
